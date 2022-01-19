d0e60945da6eae01ccdcceb7884e625bab842f86
Conner Cochrane <cccochrane1@knights.ucf.edu>
2022-01-19 16:14:48 -0500

    setting up file to connect for data

--- src/components/Space.js
+++ src/components/Space.js
@@ -1,1 +1,15 @@
 import React, {useState} from 'react';
+
+const Space = ({ data }) => {
+
+    const title = data.title
+    const explanation = data.explanation
+    const date = data.date
+    const url = item.url
+
+
+}
+
+
+
+export default Space;
