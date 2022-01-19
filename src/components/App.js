ad34c39ee6273a90c4e9ad2d324b6098d9961706
Conner Cochrane <cccochrane1@knights.ucf.edu>
2022-01-19 18:48:57 -0500

    updating the design layout

--- src/components/App.js
+++ src/components/App.js
@@ -43,20 +43,17 @@ const fetchAPI = () => {
 }
 
 return (
-    <div className='App container'>
-        <div className='head'>
-            <div className="title">
-                <h2>Spacestagram</h2>
+    <div className='heading'>
+       <h2>Spacestagram</h2>
                 
-            </div>
-        </div>
+     
     {updating ? (
         <h2 className='textloading text-center'>Loading...</h2>
       ) : (
         <div className='row'>
             {pictures.map((picture, key) => {
             return (
-             <div key={key} className='card col-lg-4 col-md-6'>
+             <div key={key} className='col-lg-6 col-md-12'>
              <Space picture={picture} />
               </div>
             );
@@ -70,137 +67,3 @@ return (
 
  }
 export default App;
-// class App extends Component{
-
-    
-// constructor(props) {
-//     super(props);
-//     this.state = {
-//         items: {},
-//         isLoaded: false,
-//     }
-// }
-
-
-// componentDidMount() {
-//     fetch(`https://api.nasa.gov/planetary/apod?api_key=${APIKey}`)
-//     .then(res => res.json())
-//     .then(json => {
-//         this.setState({
-//             isLoaded: true,
-//             items: json.items
-//         })
-//     })
-// }
-
-
-// render(){
-
-//     var { isLoaded, items } = this.state;
-//     if (!isLoaded) {
-//         return <div> processing</div>
-//     }
-//     else {
-
-//     return (
-//         <div className="App">
-//             <ul>
-//                 {items.map(item => (
-//                     <li key={item.title}>
-//                         {item.url}
-//                     </li>
-//                 ))}
-//             </ul>
-//             </div>
-//     )
-// }
-// }
-// }
-
-// // const [pictures, importPictures] = useState(null);
-// // const APIKey = "6AIGbj4jHFCcD7ChEiWMreQx3wbVgxowEsvyWVjz"
-
-// // const fetchData = async () => {
-// //     const response = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${APIKey}`)
-// //     importPictures(response.data)
-// // }
-
-
-// // return (
-// //     <div className="App">
-// //         <h1>Spacestagram</h1>
-// //         <h2>Fetch a list from an API and display it</h2>
-
-// //         {/* Fetch data from API */}
-// //         <div>
-// //           <button className="fetch-button" onClick={fetchData}>
-// //             Fetch Planets
-// //           </button>
-// //         </div>
-
-// //         {/* Display data from API */}
-// //         <div className="pictures">
-// //     {pictures && 
-// //         pictures.map((picture, index) => {
-// //             // const pictureRelease = new Date(picture.released).toDateString();
-            
-// //             return (
-// //                 <div className='picture' key={index}>
-// //                     <h4>Picture {index+1} </h4>
-// //                     <h3>{picture.title}</h3>
-
-// //                     <div className='information'>
-// //                         {/* <p>{description}</p>
-// //                         <p>{date}</p> */}
-// //                         </div>
-// //                         </div>
-// //             )
-// //         })}
-         
-// //         </div>
-
-// //       </div>
-// // )
-// // // const [rendering, nowRendered] = useState(false);
-   
-     
-// //     //   .then(response => {
-// //     //       if(!response.ok) {
-// //     //           throw Error("ERROR");
-// //     //       }
-// //     //       return response.json();
-// //     //     })
-// //     //     .then (data => {
-// //     //         console.log(data.data);
-// //     //         const html = data.data
-// //     //         .map(user => {
-// //     //             return
-// //     //         })
-// //     //     })
-
-   
-// // //       ).catch(error => {
-// // //           console.log(error)
-// // //       })
-// // //    }, []
-// // //     )
-
-// //     // useEffect(() => {
-// //     //     retreiveImage();
-
-// //     // }, [retreiveImage]);
-
-
-// //     // return (
-// //     //     <div>hello</div> 
-// //     // )
-
-
-
-// //  }
-
-
-
-// export default function App() {
-//     return <Homepage/>
-// }
--- src/components/Space.js
+++ src/components/Space.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Card} from 'react-bootstrap'
+import {Card, Button} from 'react-bootstrap'
 
 const Space = ({ picture }) => {
 
@@ -15,19 +15,31 @@ const Space = ({ picture }) => {
         setHeart(!heart);
     };
 
+    // const setLiked = () => {
+    //     this.useState({liked: this.state.liked})
+    // }
+
     return (
-        <Card style={{ width: '18rem' }}>
-  <Card.Body>
-    <Card.Title>{title}</Card.Title>
-    <Card.Subtitle className="mb-2 text-muted">{date}</Card.Subtitle>
-    <Card.Text>
+        <Card style={{ width: '90rem' }}>
+  <Card.Body className='App text-center'>
+      <br></br>
+    <Card.Title className="title"> {title}</Card.Title>
+    <br></br>
+    <Card.Subtitle className="mb-2 text-muted"> Date Taken ~ {date}</Card.Subtitle>
+    <br></br>
+    <Card.Text className='wording'>
       {explanation}
     </Card.Text>
-    <Card.Img variant="top" src={url}/>
-    <Card.Img variant="top" iframe={url}/>
-    <Card.Link href="#">Card Link</Card.Link>
-    <Card.Link href="#">Another Link</Card.Link>
-    <Button onClick={makeliked} variant="secondary"> LOVE THIS</Button>
+    <br></br>
+    <Card.Img className="img" alt="NASA PHOTO" variant="top" src={url} />
+    {/* <Card.Img className="img" alt="NASA PHOTO" variant="top" iframe={url}/> */}
+    <br></br>
+    <div>
+    <Button className="btn" onClick={makeliked}> 
+    LOVE THIS
+    {heart ? <i className="fas fa-heart"></i> : <i className="far fa-heart"></i>}
+    </Button>
+    </div>
   </Card.Body>
 </Card> 
     )
